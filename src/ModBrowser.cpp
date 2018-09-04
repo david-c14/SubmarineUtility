d9e073e6cf801007931de6f84570a8e29c7e1fb4
david-c14 <10398567+david-c14@users.noreply.github.com>
2018-09-04 19:07:06 +0100

    Refactoring to separate controls from Core Module

--- src/ModBrowser.cpp
+++ src/ModBrowser.cpp
@@ -120,7 +120,11 @@ struct TextButton : SubControls::ButtonBase {
 
 // Icons
 
-struct PluginIcon : SubControls::ButtonBase,SVGWidget {
+struct MBIconWidget : SubControls::ButtonBase,SVGWidget {
+	ModBrowserWidget *mbw;
+};
+
+struct PluginIcon : MBIconWidget {
 	ModBrowserWidget *mbw;
 	int selected = 0;
 	PluginIcon() {
@@ -130,8 +134,7 @@ struct PluginIcon : SubControls::ButtonBase,SVGWidget {
 	void onAction(EventAction &e) override;
 };
 
-struct TagIcon : SubControls::ButtonBase,SVGWidget {
-	ModBrowserWidget *mbw;
+struct TagIcon : MBIconWidget {
 	int selected = 0;
 	TagIcon() {
 		box.size.x = 30;
@@ -140,8 +143,7 @@ struct TagIcon : SubControls::ButtonBase,SVGWidget {
 	void onAction(EventAction &e) override;
 };
 
-struct FavIcon : SubControls::ButtonBase,SVGWidget {
-	ModBrowserWidget *mbw;
+struct FavIcon : MBIconWidget {
 	int selected = 0;
 	FavIcon() {
 		box.size.x = 30;
@@ -150,8 +152,7 @@ struct FavIcon : SubControls::ButtonBase,SVGWidget {
 	void onAction(EventAction &e) override;
 };
 
-struct LoadIcon : SubControls::ButtonBase,SVGWidget {
-	ModBrowserWidget *mbw;
+struct LoadIcon : MBIconWidget {
 	int selected = 0;
 	LoadIcon() {
 		box.size.x = 30;
@@ -160,8 +161,7 @@ struct LoadIcon : SubControls::ButtonBase,SVGWidget {
 	void onAction(EventAction &e) override;
 };
 
-struct MinimizeIcon : SubControls::ButtonBase,SVGWidget {
-	ModBrowserWidget *mbw;
+struct MinimizeIcon : MBIconWidget {
 	MinimizeIcon() {
 		box.size.x = 30;
 		box.size.y = 30;
